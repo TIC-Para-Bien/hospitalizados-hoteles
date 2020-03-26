8bca2ffb5f55037a2f94b9be6f21294c53f79ee1
Carlos Ble <carlos@codesai.com>
2020-03-26 14:09:55 +0000

    move folders

--- src/main/java/org/ticparabien/hotelcovid19/controller/Routes.java
+++
@@ -1,7 +0,0 @@
-package org.ticparabien.hotelcovid19.controller;
-
-public class Routes {
-    public static final String login = "/login";
-    public static final String home = "/";
-    public static final String Rooms = "/api/Rooms";
-}
--- src/test/java/org/ticparabien/hotelcovid19/helpers/Properties.java
+++
@@ -1,9 +0,0 @@
-package org.ticparabien.hotelcovid19.helpers;
-
-public class Properties {
-    public final static String WEB_SERVER_PORT = "18080";
-
-//  Real system properties should not be used when testing since
-//  conflicts with the real application or system resources may occur.
-    public final static String SYSTEM_PROPERTY = "neverUse.realSystemProperty";
-}
