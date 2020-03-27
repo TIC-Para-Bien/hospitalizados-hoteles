4f050e222a7913e81f8e06d8f0035598125f55f6
Carlos Sosa <carlos.sosa@leanmind.es>
2020-03-27 23:57:23 +0000

    Extract HealthFeedback form to components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,52 +9,50 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-        <div>
-          <section className="section">
-            <div className="container">
-              <div className="columns">
-                <div className="column">
-                  <h1 className="title">
-                    <Link to="/patients-login">Acceso Pacientes</Link>
-                  </h1>
-                </div>
-                <div className="column">
-                  <h1 className="title">
-                    <Link to="/supervisor-login">Acceso Personal Sanitario</Link>
-                  </h1>
-                </div>
-                <div className="column">
-                  <h1 className="title">
-                    <Link to="/patient-health-form">Recogida de datos de paciente</Link>
-                  </h1>
-                </div>
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column">
+                <h1 className="title">
+                  <Link to="/patients-login">Acceso Pacientes</Link>
+                </h1>
+              </div>
+              <div className="column">
+                <h1 className="title">
+                  <Link to="/supervisor-login">Acceso Personal Sanitario</Link>
+                </h1>
+              </div>
+              <div className="column">
+                <h1 className="title">
+                  <Link to="/patient-health-form">Recogida de datos de paciente</Link>
+                </h1>
               </div>
             </div>
+          </div>
 
-            <hr />
+          <hr />
 
-            <div className="container">
-              <div className="notification">
-                <h3 className="subtitle">
-                  TODO: Hacer portada con dos opciones nada mas, acceso pacientes o acceso
-                  sanitarios
-                </h3>
-              </div>
-
-              <Switch>
-                <Route exact path="/patients-login">
-                  <PatientLogin />
-                </Route>
-                <Route path="/supervisor-login">
-                  <SupervisorLogin />
-                </Route>
-                <Route path="/patient-health-form">
-                  <HealthFeedback />
-                </Route>
-              </Switch>
+          <div className="container">
+            <div className="notification">
+              <h3 className="subtitle">
+                TODO: Hacer portada con dos opciones nada mas, acceso pacientes o acceso
+                sanitarios
+              </h3>
             </div>
-          </section>
-        </div>
+
+            <Switch>
+              <Route exact path="/patients-login">
+                <PatientLogin />
+              </Route>
+              <Route path="/supervisor-login">
+                <SupervisorLogin />
+              </Route>
+              <Route path="/patient-health-form">
+                <HealthFeedback />
+              </Route>
+            </Switch>
+          </div>
+        </section>
       </Router>
     </div>
   );
