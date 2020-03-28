2b3efe51b226837bd804c302b0ad2bc559914a09
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-03-28 18:52:42 +0000

    Update: Add debug configuration to app start process

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean install
 
-java $@ -jar ./target/hotelcovid19-1.0-SNAPSHOT.jar
+java $@ -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ./target/hotelcovid19-1.0-SNAPSHOT.jar
