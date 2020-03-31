71b1835aa38e34c29d64bdf7206070c2990a4d15
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2020-03-31 19:38:16 +0100

    Implemented login of patients

--- backend/src/test/java/org/ticparabien/hotelcovid19/e2e/RegisterRoomsShould.java
+++ backend/src/test/java/org/ticparabien/hotelcovid19/e2e/RegisterRoomsShould.java
@@ -1,7 +1,6 @@
 package org.ticparabien.hotelcovid19.e2e;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
